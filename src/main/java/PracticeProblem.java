1a926d946e35d1626e15d5f820dae70157c88ac9
aarooran-anankan-ics4u-03-25 <aarooran.anankan@student.tdsb.on.ca>
2025-03-06 16:56:54 +0000

    Final Assignment

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,9 +1,63 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class PracticeProblem {
 
-	public static void main(String args[]) {
+	public static void main(String args[]) {}
 
+	public static String readFile(String Filename) {
+		
+		String readFile = "";
+		FileReader reader = null;
+
+		try {
+			reader = new FileReader(Filename);
+			int character;
+			while ((character = reader.read()) != -1) {
+				readFile = readFile + (char)character;
+			}
+		}
+
+		catch (IOException e) {}
+
+		finally {
+			try {
+				if (reader != null) {
+					reader.close();
+				}
+			}
+			catch (IOException e) {}
+		}
+
+		return readFile;
 	}
 
-	
+	public static String backwardsReadFile(String filename) {
+
+		String readFile = "";
+		FileReader reader = null;
+
+		try {
+			reader = new FileReader(filename);
+			int character;
+			while ((character = reader.read()) != -1) {
+				readFile = (char)character + readFile;
+			}
+		}
+
+		catch (IOException e) {}
+
+		finally {
+			try {
+				if (reader != null) {
+					reader.close();
+				}
+			}
+			catch (IOException e) {}
+		}
+
+		return readFile;
+
+	}
 
 }
